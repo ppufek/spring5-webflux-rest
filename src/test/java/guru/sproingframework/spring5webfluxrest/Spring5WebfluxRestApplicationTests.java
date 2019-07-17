4b7da084b92f7256de1e41b757482b9d1917e485
epufpau <paula.pufek@ericsson.com>
2019-07-17 11:21:46 +0200

    CircleCI, again

--- src/test/java/guru/sproingframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
+++ src/test/java/guru/sproingframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -1,6 +1,6 @@
 package guru.sproingframework.spring5webfluxrest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
