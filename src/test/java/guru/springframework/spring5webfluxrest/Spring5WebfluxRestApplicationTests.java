d488bfc94528870e29ade8206a23c2a5ee0bccd3
epufpau <paula.pufek@ericsson.com>
2019-07-17 11:38:56 +0200

    Again...

--- src/test/java/guru/springframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
+++ src/test/java/guru/springframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -1,13 +1,16 @@
-package guru.sproingframework.spring5webfluxrest;
+package guru.springframework.spring5webfluxrest;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class Spring5WebfluxRestApplicationTests {
+public class Spring5WebfluxRestApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
